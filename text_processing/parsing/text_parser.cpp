d20fa433bae9a211a6d12ea66e57ddd7bfacfedf
aleksandr.biryukov <aleksandr.biryukov@partner.bmw.de>
2021-08-26 17:35:12 +0300

    Ghost layering fix

--- text_processing/parsing/text_parser.cpp
+++ text_processing/parsing/text_parser.cpp
@@ -22,11 +22,13 @@ void TextParser::Parse(std::vector<Layer>& result)
 			Layer new_layer{
 				{},
 				0,
-				it == mDataView.Begin(),
-				true
+				true,
+				it < mDataView.Begin() + mMask.size(),
+				false
 			};
 
 			ProcessNewLayer(new_layer, it, mDataView.End());
+			new_layer.ends_in_layering_range = it - mDataView.Begin() <= mMask.size();
 			result.emplace_back(std::move(new_layer));
 		}
 	}
@@ -66,69 +68,6 @@ void TextParser::ProcessNewLayer(Layer& result, IteratorType& position, const It
 		++position;
 	}
 }
-
-// void TextParser::Parse(std::vector<Layer>& result)
-// {
-// 	if(!mDataView.Empty())
-// 	{
-// 		auto mask_iter = mMask.cbegin();
-// 		size_t new_layer_distance{0};
-// 		TextDataView::IteratorType it = mDataView.Begin();
-// 		for(size_t i = 0; i < mDataView.Size(); ++i)
-// 		{
-// 			it = mDataView.Begin() + i;
-// 			if(result.empty())
-// 			{
-// 				result.emplace_back(
-// 					Layer{
-// 						std::map<size_t, std::string>(),
-// 						0,
-// 						true,
-// 						true
-// 					}
-// 				);
-// 			}
-
-// 			if(*it == '\n')
-// 			{
-// 				result.back().length = it - mDataView.Begin() - new_layer_distance;
-// 				result.back().incomplete_from_end = false;
-// 				result.emplace_back(
-// 					Layer{
-// 						std::map<size_t, std::string>(),
-// 						0,
-// 						false,
-// 						true
-// 					}
-// 				);
-// 				new_layer_distance = i + 1;
-// 			}
-// 			// std::cout << ((*it) + " ");
-// 			if(Compare(*mask_iter, *it))
-// 			{
-// 				// std::cout << (std::to_string(i) + " " + *it + "\n");
-// 				if(mask_iter + 1 == mMask.cend())
-// 				{
-// 					std::string found_word{it - mMask.size() + 1, it + 1};
-// 					// std::cout << ("Found: " + found_word + "\n");
-// 					size_t distance = i - new_layer_distance;
-// 					result.back().appearences.insert(std::make_pair<size_t, std::string>(std::move(distance), std::move(found_word)));
-// 					mask_iter = mMask.cbegin();
-// 				}
-// 				else
-// 				{
-// 					++mask_iter;
-// 				}
-
-// 			}
-// 			else if(mask_iter != mMask.cbegin())
-// 			{
-// 				mask_iter = mMask.cbegin();
-// 			}
-// 		}
-// 		if(!result.empty()) result.back().length = (*it != '\n') ? it - mDataView.Begin() - new_layer_distance + 1 : it - mDataView.Begin() - new_layer_distance;
-// 	}
-// }
 
 bool TextParser::Compare(char mask, char symbol) const
 {
--- text_processing/parsing/text_parser.hpp
+++ text_processing/parsing/text_parser.hpp
@@ -19,16 +19,17 @@ struct Layer
 	*/
 	std::map<size_t, std::string> appearences;
 	size_t length;
-	bool incomplete_from_begin;
 	bool incomplete_from_end;
+	bool stars_in_layering_range;
+	bool ends_in_layering_range;
 
-	void Merge(const Layer& other, size_t layering)
+	void Merge(const Layer& other, size_t layering, size_t appearence_size)
 	{
 		for(auto it = other.appearences.cbegin(); it != other.appearences.cend(); ++it)
 		{
-			auto distance = it->first + length - (length > 0 ? layering : 0);
+			size_t distance = it->first + length - (length > 0 ? layering : 0);
 			const auto& word = it->second;
-			if(appearences.empty() || (distance - appearences.crbegin()->first >= layering))
+			if(appearences.empty() || (distance >= appearences.crbegin()->first + appearence_size))
 			{
 				appearences.insert(std::pair<size_t, std::string>(distance, word));
 			}
@@ -40,8 +41,9 @@ struct Layer
 	{
 		appearences.clear();
 		length = 0;
-		incomplete_from_begin = false;
 		incomplete_from_end = false;
+		stars_in_layering_range = false;
+		ends_in_layering_range = false;
 	}
 };
 
@@ -66,16 +68,23 @@ struct ParsingResult
 		std::ostringstream result;
 		std::vector<Layer> layers;
 		GetMergedLayerSequence(layers);
-		for(size_t i = 0; i < layers.size(); ++i)
-		{
-			if(!layers[i].appearences.empty())
-			{
-				for(const auto& entry : layers[i].appearences)
-				{
-					result << i + 1 << " " << entry.first << " " << entry.second << "\n";
-				}
-			}
-		}
+		std::cout << layers.size();
+		// size_t acc = 0;
+		// for(const auto& entry : mResultSequence)
+		// {
+		// 	acc += entry.second.size();
+		// }
+		// std::cout << acc << std::endl;
+		// for(size_t i = 0; i < layers.size(); ++i)
+		// {
+		// 	if(!layers[i].appearences.empty())
+		// 	{
+		// 		for(const auto& entry : layers[i].appearences)
+		// 		{
+		// 			result << i + 1 << " " << entry.first << " " << entry.second << "\n";
+		// 		}
+		// 	}
+		// }
 		return result.str();
 	}
 
@@ -84,102 +93,41 @@ struct ParsingResult
 		using SeqIteratorType = std::map<size_t, std::vector<Layer>>::const_iterator;
 		if(!mResultSequence.empty())
 		{
-			Layer current_line{{}, 0, false, false};
+			Layer current_line{{}, 0, false, false, false};
+			size_t ghost_layering{0};
 			for(SeqIteratorType it = mResultSequence.cbegin(); it != mResultSequence.cend(); ++it)
 			{
-				const auto idx = it->first;
 				const auto& layers = it->second;
 				for(auto l_it = layers.cbegin(); l_it != layers.cend(); ++l_it)
 				{
-					current_line.Merge(*l_it, mLayeringSize);
-					if(!l_it->incomplete_from_end || (++SeqIteratorType(it) == mResultSequence.cend() && l_it + 1 == layers.cend()))
+					bool is_ghost_layer = l_it->stars_in_layering_range && l_it->ends_in_layering_range;
+					if(is_ghost_layer)
 					{
-						merged_layers.emplace_back(std::move(current_line));
-						current_line.Reset();
+						ghost_layering += (l_it->length + 1);
+					}
+					else
+					{
+						current_line.Merge(*l_it, mLayeringSize - ghost_layering, mLayeringSize);
+						if(!l_it->incomplete_from_end || (++SeqIteratorType(it) == mResultSequence.cend() && l_it + 1 == layers.cend()))
+						{
+							merged_layers.emplace_back(std::move(current_line));
+							current_line.Reset();
+						}
+						ghost_layering = 0;
 					}
 				}
 			}
 		}
 	}
 
-	// std::string GetValue()
-	// {
-	// 	std::ostringstream result;
-	// 	if(!mLayers.empty())
-	// 	{
-	// 		using LayerIterator = std::vector<Layer>::const_iterator;
-	// 		LayerIterator current, prev;
-	// 		auto current_layer = mLayers.cbegin();
-	// 		current = prev = current_layer->second.cbegin();
-	// 		std::string layer_result;
-	// 		Layer real_line;
-	// 		size_t real_lines_count;
-	// 		size_t current_line_idx{1};
-	// 		while(true)
-	// 		{
-	// 			if(current != prev)
-	// 			{
-	// 				bool prev_incomplete = (*prev).incomplete_from_end;
-	// 				bool curr_incomplete = (*current).incomplete_from_begin;
-	// 				bool part_of_prev = prev_incomplete && curr_incomplete;
-	// 				assert((prev_incomplete && curr_incomplete) || (!prev_incomplete && !curr_incomplete));
-
-	// 				if(part_of_prev)
-	// 				{
-	// 					 for(const auto& appearence : current->appearences)
-	// 					 {
-	// 					 	real_line.appearences[real_line.length + appearence.first] = appearence.second;
-	// 					 }
-	// 					 real_line.length += (current->length - mLayeringSize);
-	// 				}
-	// 				else
-	// 				{
-	// 					if(!real_line.appearences.empty())
-	// 					{
-	// 						for(const auto& word : real_line.appearences)
-	// 						{
-	// 							result << current_line_idx << " " << word.first << " " << word.second << "\n";
-	// 						}
-	// 					}
-	// 					real_line = *current;
-	// 				}
-	// 			}
-	// 			else
-	// 			{
-	// 				real_line = *prev;
-	// 			}
-
-	// 			prev = current;
-	// 			if(++current == current_layer->second.cend())
-	// 			{
-	// 				if(++current_layer == mLayers.cend())
-	// 				{
-	// 					if(!real_line.appearences.empty())
-	// 					{
-	// 						for(const auto& word : real_line.appearences)
-	// 						{
-	// 							result << current_line_idx << " " << word.first << " " << word.second << "\n";
-	// 						}
-	// 					}
-	// 					break;
-	// 				}
-	// 				else
-	// 				{
-	// 					current = current_layer->second.cbegin();
-	// 				}
-	// 			}
-	// 			++current_line_idx;
-	// 		}
-
-	// 	}
-	// 	return result.str();
-	// }
-
 	/*
 	Raw data, need to merge layers before read,
 	first - order of data chunk, second - layers(lines) of text
 	*/
 	std::map<size_t, std::vector<Layer>> mResultSequence;
+	/*
+	Layering size == mask size
+	*/
 	size_t mLayeringSize;
 };
 
